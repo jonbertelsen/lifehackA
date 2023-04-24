a833e364148cbec51f0c89ebd54402e61c89e1cf
MagKon <magnus.kongstad@gmail.com>
2023-04-24 11:54:07 +0200

    Added the beginning of the code

--- src/main/java/dat/backend/control/guru/GuruLoad.java
+++ src/main/java/dat/backend/control/guru/GuruLoad.java
@@ -1,4 +1,42 @@
 package dat.backend.control.guru;
 
-public class GuruLoad {
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@WebServlet(name = "guru", urlPatterns = {"/guru\""} )
+public class GuruLoad extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        String url = "https://api.openai.com/v1/completions";
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", "Bearer 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");
+
+        JSONObject data = new JSONObject();
+        data.put("model", "text-davinci-003");
+        data.put("prompt", text);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+
+        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
+                .reduce((a, b) -> a + b).get();
+
+        System.out.println(new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text"));
+    }
 }
