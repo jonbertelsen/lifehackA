246676c8193416c7a50fa6ea312b488c9d908832
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2023-04-24 19:48:56 +0200

    Removed unused import..

--- src/test/java/dat/backend/control/BMICalculatorTest.java
+++ src/test/java/dat/backend/control/BMICalculatorTest.java
@@ -3,8 +3,6 @@ package dat.backend.control;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
