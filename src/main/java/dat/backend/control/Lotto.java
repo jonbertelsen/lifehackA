b69e7b0f23471ea7e0805eb56780256ca3e051ee
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-24 14:24:56 +0200

    backend for Lotto Coupons made

--- src/main/java/dat/backend/control/Lotto.java
+++ src/main/java/dat/backend/control/Lotto.java
@@ -15,6 +15,84 @@ public class Lotto extends HttpServlet {
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        LottoCouponInformation information = (LottoCouponInformation) request.getAttribute("lottoCouponInformation");
+        if (information == null) {
+            information = new LottoCouponInformation();
+        }
 
+        if (request.getParameter("numberCount") != null) {
+            information.setNumberCount(Integer.parseInt(request.getParameter("numberCount")));
+        }
+
+        if (request.getParameter("numberIntervalMin") != null) {
+            information.setNumberIntervalMin(Integer.parseInt(request.getParameter("numberIntervalMin")));
+        }
+
+        if (request.getParameter("numberIntervalMax") != null) {
+            information.setNumberIntervalMax(Integer.parseInt(request.getParameter("numberIntervalMax")));
+        }
+
+        if (request.getParameter("ticketCount") != null) {
+            information.setTicketCount(Integer.parseInt(request.getParameter("ticketCount")));
+        }
+
+        if (information.getNumberIntervalMin() > information.getNumberIntervalMax()) {
+            request.setAttribute("error", "Minimum number must be smaller than maximum number");
+        }
+
+        request.setAttribute("lottoCouponInformation", information);
+        request.getRequestDispatcher("lotto.jsp").forward(request, response);
+    }
+
+    public static class LottoCouponInformation {
+
+        private int numberCount = 7;
+        private int numberIntervalMin = 1;
+        private int numberIntervalMax = 36;
+        private int ticketCount = 10;
+
+        public LottoCouponInformation() { }
+
+        public int getNumberCount() {
+            return this.numberCount;
+        }
+
+        public void setNumberCount(int numberCount) {
+            this.numberCount = numberCount;
+        }
+
+        public int getNumberIntervalMin() {
+            return this.numberIntervalMin;
+        }
+
+        public void setNumberIntervalMin(int numberIntervalMin) {
+            this.numberIntervalMin = numberIntervalMin;
+        }
+
+        public int getNumberIntervalMax() {
+            return this.numberIntervalMax;
+        }
+
+        public void setNumberIntervalMax(int numberIntervalMax) {
+            this.numberIntervalMax = numberIntervalMax;
+        }
+
+        public int getTicketCount() {
+            return this.ticketCount;
+        }
+
+        public void setTicketCount(int ticketCount) {
+            this.ticketCount = ticketCount;
+        }
+
+        public int[][] generateTickets() {
+            int[][] tickets = new int[this.ticketCount][this.numberCount];
+            for (int i = 0; i < this.ticketCount; i++) {
+                for (int j = 0; j < this.numberCount; j++) {
+                    tickets[i][j] = (int) (Math.random() * (this.numberIntervalMax - this.numberIntervalMin + 1)) + this.numberIntervalMin;
+                }
+            }
+            return tickets;
+        }
     }
 }
