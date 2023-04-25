ab9e8e5e6e56be0551a59b7fec1eac3b939c119e
Peter537 <pean@outlook.dk>
2023-04-25 12:42:01 +0200

    mdae classes not public

--- src/test/java/dat/backend/entities/lotto/LottoCouponTest.java
+++ src/test/java/dat/backend/entities/lotto/LottoCouponTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LottoCouponTest {
+class LottoCouponTest {
 
     private LottoCoupon lottoCoupon;
 
--- src/test/java/dat/backend/entities/lotto/LottoNumberTest.java
+++ src/test/java/dat/backend/entities/lotto/LottoNumberTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LottoNumberTest {
+class LottoNumberTest {
 
     private LottoNumber lottoNumber;
 
