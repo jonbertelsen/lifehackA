8301255aa62d937cfce18e5c88e77b8d8507ece4
JuliusLansner <axiiss@live.dk>
2023-04-25 11:26:32 +0200

    Update BMICalculator.java

--- src/main/java/dat/backend/model/entities/BMICalculator.java
+++ src/main/java/dat/backend/model/entities/BMICalculator.java
@@ -7,7 +7,7 @@ public class BMICalculator {
     public static void main(String[] args) {
 
     }
-
+//
     public double calculateBMI (double weightInKg, double heightInCm){
 
         double heightInM = heightInCm / 100;
