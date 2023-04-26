3731f2eb4f97a089f4da4dbcf68799f8401e706f
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2023-04-26 13:28:13 +0200

    JavaDoc for unwantedNumbers.java

--- src/main/java/dat/backend/control/lotto/UnwantedNumbers.java
+++ src/main/java/dat/backend/control/lotto/UnwantedNumbers.java
@@ -7,8 +7,21 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
+/**
+ * The purpose of this whole servlet is to handle the user's unwanted numbers, that are to be excluded from the lottery generation
+ *
+ */
+
 @WebServlet(name = "unwanted-numbers", value = "/unwanted-numbers")
 public class UnwantedNumbers extends HttpServlet {
+
+
+    /**
+     * Incase the user regret a decision or accidentally typed the wrong number to exclude from the lottery,
+     * this get-method will remove the number from the list of {@link LottoCoupon}'s unwanted numbers.
+     *
+     * @author Yusuf
+     */
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -36,6 +49,12 @@ public class UnwantedNumbers extends HttpServlet {
 
 
     }
+
+    /**
+     * This post-method's purpose is to load in the {@link LottoCoupon}-object and add the unwanted numbers the user inputted on the prior page
+     * to the unwanted numbers list. This list then will be loaded on the iframe on said page.
+     * @author Yusuf
+     */
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
