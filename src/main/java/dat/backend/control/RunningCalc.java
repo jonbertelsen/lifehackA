d0cf98b062524ae1e5e892bca5d7be1b720f410a
SitthichaiPhuanphimai <sphuanphimai@gmail.com>
2023-04-26 14:11:01 +0200

    deleted comments

--- src/main/java/dat/backend/control/RunningCalc.java
+++ src/main/java/dat/backend/control/RunningCalc.java
@@ -15,28 +15,36 @@ public class RunningCalc extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
+        /**
+         * formType gets the value from runningCalc.jsp to determine which calculations will be made in the  servlet
+         */
         String formType = request.getParameter("formType");
 
-        //gets parameter from form "runnng time"
+        /**
+         * If the user uses the form with value "calcTime", this code will  run
+         */
         if(formType.equals("calcTime")) {
             int minutes = Integer.parseInt(request.getParameter("minutes"));
             int seconds = Integer.parseInt(request.getParameter("seconds"));
             int distance = Integer.parseInt(request.getParameter("distance"));
 
-            //calculate total second to hours, minutes and second
+
             int totalSeconds = distance * ((minutes * 60) + seconds);
 
             int tHours = totalSeconds / 3600;
             int tMinutes = (totalSeconds % 3600) / 60;
             int tSeconds = totalSeconds % 60;
 
-            //format the variables to display a time format in HH:MM:SS
+            /**
+             * totalTime formats  the String with the arguments tHours,tMinutes and paceSec, and display it in a time format that is xx hours xx minutes and xx seconds
+             */
             String totalTime = String.format(" %d hours, %d minutes, %d seconds", tHours, tMinutes, tSeconds);
 
             request.setAttribute("totalTime", totalTime);
         }
-
+        /**
+         * if the formType is "calcPace", this code would run
+         */
         else if(formType.equals("calcPace")) {
 
 
@@ -51,6 +59,9 @@ public class RunningCalc extends HttpServlet {
             int paceMin =  pace/60;
             int paceSec =  pace%60;
 
+            /**
+             * totalPace formats a String that takes in paceMin and paceSec, and display it in xx minutes and xx seconds
+             */
             String totalPace = String.format(" %d minutes, %d seconds",paceMin, paceSec);
 
             request.setAttribute("totalPace", totalPace);
