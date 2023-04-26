e2c4edb2aee2b92fdb313e1ba304572c9031997c
Stevespecious <g4m3rkc@gmail.com>
2023-04-26 13:24:48 +0200

    Create package-info.java

---
+++ src/main/java/dat/backend/control/unitConverter/package-info.java
@@ -0,0 +1,6 @@
+package dat.backend.control.unitConverter;
+/**
+ * The purpose of these two classes (converterCalc, unitConverter) is to make it possible for the user to
+ * enter a specific number in any of the 8 unit types. It will then automatically converts the userinput into
+ * the selected output unit type - and show the user the output.
+ */
