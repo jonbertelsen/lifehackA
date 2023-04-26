5ba2031eac67e455a11cbdb8b69b93349ae3b5bc
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-26 13:24:29 +0200

    java doc to entities

--- src/main/java/dat/backend/model/entities/lotto/LottoCoupon.java
+++ src/main/java/dat/backend/model/entities/lotto/LottoCoupon.java
@@ -2,6 +2,10 @@ package dat.backend.model.entities.lotto;
 
 import java.util.*;
 
+/**
+ * The {@code LottoCoupon} class represents a coupon for a lottery game, containing multiple {@link LottoRow}.
+ * @author Peter
+ */
 public class LottoCoupon {
 
     private final Map<Integer, LottoRow> rows = new HashMap<>();
@@ -12,6 +16,16 @@ public class LottoCoupon {
     private int numberIntervalMax = 36;
     private int ticketCount = 10;
 
+    /**
+     * Constructs a {@link LottoCoupon} object with the given parameters.
+     *
+     * @param numberCount The number of LottoNumbers in each LottoRow.
+     * @param numberIntervalMin The minimum value for each LottoNumber in a LottoRow.
+     * @param numberIntervalMax The maximum value for each LottoNumber in a LottoRow.
+     * @param ticketCount The number of LottoRows in the LottoCoupon.
+     * @param unwantedNumbers The set of unwanted numbers that should not be present in any {@link LottoRow}.
+     * @param rows The map of LottoRows for this {@link LottoCoupon}.
+     */
     public LottoCoupon(int numberCount, int numberIntervalMin, int numberIntervalMax, int ticketCount, Set<Integer> unwantedNumbers, Map<Integer, LottoRow> rows) {
         this.numberCount = numberCount;
         this.numberIntervalMin = numberIntervalMin;
@@ -21,6 +35,14 @@ public class LottoCoupon {
         this.rows.putAll(rows);
     }
 
+    /**
+     * Constructs a {@link LottoCoupon} object with the given parameters, and no rows.
+     *
+     * @param numberCount The number of LottoNumbers in each LottoRow.
+     * @param numberIntervalMin The minimum value for each LottoNumber in a LottoRow.
+     * @param numberIntervalMax The maximum value for each LottoNumber in a LottoRow.
+     * @param ticketCount The number of LottoRows in the LottoCoupon.
+     */
     public LottoCoupon(int numberCount, int numberIntervalMin, int numberIntervalMax, int ticketCount) {
         this.numberCount = numberCount;
         this.numberIntervalMin = numberIntervalMin;
@@ -28,8 +50,17 @@ public class LottoCoupon {
         this.ticketCount = ticketCount;
     }
 
+    /**
+     * Constructs an empty {@link LottoCoupon} object.
+     */
     public LottoCoupon() { }
 
+    /**
+     * Generates a {@link LottoCoupon} based on specific criteria.
+     *
+     * @author Peter
+     * @return The {@link LottoCoupon} with generated rows (if necessary).
+     */
     public LottoCoupon generateCoupon() {
         if (this.rows.size() == this.ticketCount) {
             return this;
@@ -38,6 +69,12 @@ public class LottoCoupon {
         return regenerateCoupon();
     }
 
+    /**
+     * Helper method that generates a {@link LottoCoupon} based on specific criteria.
+     *
+     * @author Peter
+     * @return The {@link LottoCoupon} with regenerated rows.
+     */
     private LottoCoupon regenerateCoupon() {
         Map<Integer, LottoRow> rows = new HashMap<>();
         for (int i = 1; i <= this.ticketCount; i++) {
@@ -49,58 +86,131 @@ public class LottoCoupon {
         return this;
     }
 
+    /**
+     * Returns the number of {@link LottoNumber}s in each {@link LottoRow}.
+     *
+     * @return The number of {@link LottoNumber}s in each {@link LottoRow}.
+     */
     public int getNumberCount() {
         return this.numberCount;
     }
 
+    /**
+     * Sets the number of {@link LottoNumber}s in each {@link LottoRow}.
+     *
+     * @param numberCount The number of {@link LottoNumber}s in each {@link LottoRow}.
+     */
     public void setNumberCount(int numberCount) {
         this.numberCount = numberCount;
     }
 
+    /**
+     * Returns the minimum value for each {@link LottoNumber} in a {@link LottoRow}.
+     *
+     * @return The minimum value for each {@link LottoNumber} in a {@link LottoRow}.
+     */
     public int getNumberIntervalMin() {
         return this.numberIntervalMin;
     }
 
+    /**
+     * Sets the minimum value for each {@link LottoNumber} in a {@link LottoRow}.
+     *
+     * @param numberIntervalMin The minimum value for each {@link LottoNumber} in a {@link LottoRow}.
+     */
     public void setNumberIntervalMin(int numberIntervalMin) {
         this.numberIntervalMin = numberIntervalMin;
     }
 
+    /**
+     * Returns the maximum value for each {@link LottoNumber} in a {@link LottoRow}.
+     *
+     * @return The maximum value for each {@link LottoNumber} in a {@link LottoRow}.
+     */
     public int getNumberIntervalMax() {
         return this.numberIntervalMax;
     }
 
+    /**
+     * Sets the maximum value for each {@link LottoNumber} in a {@link LottoRow}.
+     *
+     * @param numberIntervalMax The maximum value for each {@link LottoNumber} in a {@link LottoRow}.
+     */
     public void setNumberIntervalMax(int numberIntervalMax) {
         this.numberIntervalMax = numberIntervalMax;
     }
 
+    /**
+     * Returns the number of {@link LottoRow}s in the {@link LottoCoupon}.
+     *
+     * @return The number of {@link LottoRow}s in the {@link LottoCoupon}.
+     */
     public int getTicketCount() {
         return this.ticketCount;
     }
 
+    /**
+     * Sets the number of {@link LottoRow}s in the {@link LottoCoupon}.
+     *
+     * @param ticketCount The number of {@link LottoRow}s in the {@link LottoCoupon}.
+     */
     public void setTicketCount(int ticketCount) {
         this.ticketCount = ticketCount;
     }
 
+    /**
+     * Returns the set of unwanted numbers that should not be present in any {@link LottoRow}.
+     *
+     * @return The set of unwanted numbers that should not be present in any {@link LottoRow}.
+     */
     public Set<Integer> getUnwantedNumbers() {
         return this.unwantedNumbers;
     }
 
+    /**
+     * Adds an unwanted number to the set of unwanted numbers.
+     *
+     * @param number The number to add to the set of unwanted numbers.
+     * @return True if the number was added, false otherwise.
+     */
     public boolean addUnwantedNumber(int number) {
         return this.unwantedNumbers.add(number);
     }
 
+    /**
+     * Removes an unwanted number from the set of unwanted numbers.
+     *
+     * @param number The number to remove from the set of unwanted numbers.
+     * @return True if the number was removed, false otherwise.
+     */
     public boolean removeUnwantedNumber(int number) {
         return this.unwantedNumbers.remove(number);
     }
 
+    /**
+     * Returns the map of {@link LottoRow}s for this {@link LottoCoupon}.
+     *
+     * @return The map of {@link LottoRow}s for this {@link LottoCoupon}.
+     */
     public Map<Integer, LottoRow> getRows() {
         return this.rows;
     }
 
+    /**
+     * Adds a {@link LottoRow} to the map of {@link LottoRow}s for this {@link LottoCoupon}.
+     *
+     * @param rowNumber The key for the {@link LottoRow}.
+     * @param row The {@link LottoRow} to add to the map.
+     */
     public void addRow(int rowNumber, LottoRow row) {
         this.rows.put(rowNumber, row);
     }
 
+    /**
+     * Removes a {@link LottoRow} from the map of {@link LottoRow}s for this {@link LottoCoupon}.
+     *
+     * @param rowNumber The key for the {@link LottoRow} to remove.
+     */
     public void removeRow(int rowNumber) {
         this.rows.remove(rowNumber);
     }
--- src/main/java/dat/backend/model/entities/lotto/LottoNumber.java
+++ src/main/java/dat/backend/model/entities/lotto/LottoNumber.java
@@ -2,16 +2,40 @@ package dat.backend.model.entities.lotto;
 
 import java.util.Set;
 
+/**
+ * The {@code LottoNumber} class represents a lotto number.
+ * @author Peter
+ */
 public class LottoNumber {
 
+    /**
+     * The integer number that represents the {@link LottoNumber}.
+     */
     private int number;
 
+    /**
+     * Constructs a {@link LottoNumber} with the given number.
+     *
+     * @param number the number that represents the lotto number.
+     */
     public LottoNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * Constructs an empty {@link LottoNumber}
+     */
     public LottoNumber() { }
 
+    /**
+     * Generates a {@link LottoNumber} based on specific criteria.
+     *
+     * @param numberIntervalMin the minimum number in the number interval.
+     * @param numberIntervalMax the maximum number in the number interval.
+     * @param unwantedNumbers   a set of unwanted numbers that should not be generated.
+     * @author Peter
+     * @return the generated {@link LottoNumber}.
+     */
     public LottoNumber generateNumber(int numberIntervalMin, int numberIntervalMax, Set<Integer> unwantedNumbers) {
         if (this.number >= numberIntervalMin && this.number <= numberIntervalMax && !unwantedNumbers.contains(this.number)) {
             return this;
@@ -20,6 +44,15 @@ public class LottoNumber {
         return regenerateNumber(numberIntervalMin, numberIntervalMax, unwantedNumbers);
     }
 
+    /**
+     * Helper method that generates a {@link LottoNumber} based on specific criteria.
+     *
+     * @param numberIntervalMin the minimum number in the number interval.
+     * @param numberIntervalMax the maximum number in the number interval.
+     * @param unwantedNumbers   a set of unwanted numbers that should not be generated.
+     * @author Peter
+     * @return the regenerated {@link LottoNumber}.
+     */
     private LottoNumber regenerateNumber(int numberIntervalMin, int numberIntervalMax, Set<Integer> unwantedNumbers) {
         while (true) {
             int currentNum = (int) (Math.random() * (numberIntervalMax - numberIntervalMin + 1)) + numberIntervalMin;
@@ -30,10 +63,20 @@ public class LottoNumber {
         }
     }
 
+    /**
+     * Returns the number that represents the {@link LottoNumber}.
+     *
+     * @return the number that represents the {@link LottoNumber}.
+     */
     public int getNumber() {
         return this.number;
     }
 
+    /**
+     * Sets the number that represents the {@link LottoNumber}.
+     *
+     * @param number the number that represents the {@link LottoNumber}.
+     */
     public void setNumber(int number) {
         this.number = number;
     }
